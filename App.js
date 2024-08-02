ea40c2a2dcc6e38e64bd55c95ed82a6e0d4e70d4
SaiAshish <saiashish3760@gmail.com>
2024-08-02 11:24:53 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,35 @@
+import Modal from "./Modal";
+import { useState } from "react";
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div
+      className="app"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button onClick={openModal}>
+        Open Modal
+      </button>
+      <div id="modal-root"></div> 
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <h2>This is a modal!</h2>
+          <button onClick={closeModal}>Close</button>
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default App;
